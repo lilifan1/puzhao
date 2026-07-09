cc6465eb8963a23c0f2853a81c7663bda1315a21
lilifan1 <greattrading6@gmail.com>
2026-07-09 15:00:08 +0400

    fix: 返回按钮强制回到迅搜页面并保留搜索状态

--- src/views/FullSearch.vue
+++ src/views/FullSearch.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="full-search-container">
     <div class="nav-bar">
-      <button @click="goBack" class="back-btn">🏠 返回</button>
+      <button @click="goBack" class="back-btn">← 返回</button>
       <span class="title">全文搜索</span>
       <button @click="refreshIframe" class="refresh-btn">🔄 刷新</button>
     </div>
